2a83a37182acb76a2b59a2b87f0e36e595f5d7f7
huang6349 <252683858@qq.com>
2019-12-07 21:35:24 +0800

    改进建筑扫描的代码结构

--- src/pages/map/components/MidRightBar/ScanControl/index.js
+++ src/pages/map/components/MidRightBar/ScanControl/index.js
@@ -1,55 +1,57 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { useList } from 'react-use';
+import { useMap } from 'react-use';
 import { Button, Tooltip } from 'antd';
 import * as turf from '@turf/turf';
 import { IconFont } from '@/components';
 
 export default function ScanControlView({ loading, isScan, map, onClick }) {
-  const [scanFeatures, { set }] = useList([]);
+  const [{ features, filter }, { set }] = useMap({ radius: 0.0 });
 
-  React.useLayoutEffect(() => {
-    if (!map || !isScan) return;
-
+  React.useEffect(() => {
+    if (!map) return;
     const center = turf.point([114.0213, 22.5523]);
     const features = map.querySourceFeatures('buildings', { sourceLayer: 'buildings' }).map((feature) => ({
       id: feature['id'],
       distance: turf.distance(center, turf.center(feature)),
     }));
+    set('features', features);
+    return () => set('features', []);
+  }, [map]);
+
+  React.useEffect(() => {
+    if (!map || !filter) return;
+    const fn = (id, highlighted) => {
+      map.setFeatureState({ id, source: 'buildings', sourceLayer: 'buildings' }, { highlighted });
+    };
+    filter.forEach(({ id }) => fn(id, !0));
+    return () => filter.forEach(({ id }) => fn(id, !1));
+  }, [map, filter]);
+
+  React.useLayoutEffect(() => {
+    if (!map || !features || !isScan) return;
     const speedFactor = 30;
     let animation;
     let startTime = 0;
     let radius = 0.0;
 
-    const startAnimation = (timestamp) => {
+    const updateFrame = (timestamp) => {
       const progress = timestamp - startTime;
       if (progress > speedFactor) {
         startTime = timestamp;
-        set(features.filter(({ distance }) => distance < radius && distance > radius - 0.1));
+        const filter = features.filter(({ distance }) => distance < radius && distance > radius - 0.1);
         radius = radius >= 10.0 ? 0.0 : radius + 0.2;
+        set('filter', filter);
       }
-      animation = requestAnimationFrame(startAnimation);
+      animation = requestAnimationFrame(updateFrame);
     };
-    startTime = performance.now();
-    startAnimation(startTime);
+    animation = requestAnimationFrame(updateFrame);
 
     return () => {
       cancelAnimationFrame(animation);
-      set([]);
+      set('filter', []);
     };
-  }, [isScan, map]);
-
-  React.useEffect(() => {
-    if (!map) return;
-    scanFeatures.forEach(({ id }) => {
-      map.setFeatureState({ id, source: 'buildings', sourceLayer: 'buildings' }, { highlighted: !0 });
-    });
-    return () => {
-      scanFeatures.forEach(({ id }) => {
-        map.setFeatureState({ id, source: 'buildings', sourceLayer: 'buildings' }, { highlighted: !1 });
-      });
-    };
-  }, [scanFeatures, map]);
+  }, [map, features, isScan]);
 
   return (
     <Tooltip title={`${!isScan ? '开启' : '关闭'}建筑扫描`} placement="left">
